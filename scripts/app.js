8e1dc1a6f46ff7a45d4d1014f97e795a05ddc4da
dipolack <pol22007@byui.edu>
2023-01-11 02:26:46 -0500

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector('h1');
+const copyright = document.querySelector('#copyright');
+
+h1.textContent = `Dorca I Polack`;
+
+copyright.textcontent = ` ${new Date() .getFullYear()}`;
+let quantity = document.querySelector('#q').value;
