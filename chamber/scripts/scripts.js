905b47558208cfa8ed57c1f6bfef388c7cc71114
dipolack <pol22007@byui.edu>
2023-01-30 11:33:41 -0500

    adding

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,34 @@
+date = new Date(Date.now());
+document.querySelector('#©').textContent = date.getFullYear();
+
+const options = 
+{ weekday: 'long', 
+year: 'numeric', 
+month: 'numeric', 
+day: 'numeric' };
+
+document.querySelector('#dateMod').textContent = 
+new Date(Date.now()).toLocaleString();
+
+const options2 = 
+{ weekday: 'long', 
+year: 'numeric', 
+month: 'long', 
+day: 'numeric' };
+
+
+document.querySelector('#datetime').textContent = 
+new Date(Date.now()).toLocaleString('en-UK',options2);
+
+function toggleMenu() {
+    document.getElementById('primaryNav').classList.toggle('open');
+    document.getElementById('hamburguerBtn').classList.toggle('open');
+}
+
+const x = document.getElementById('hamburguerBtn');
+
+x.onclick = toggleMenu;
+
+article = document.querySelector('#message');
+
+weekday = date.getDay();
