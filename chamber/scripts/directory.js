90fffcaf802763d5b5baad90ac280ace752b0508
dipolack <pol22007@byui.edu>
2023-03-09 07:06:07 -0500

    changes

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,4 @@
-const reqtURL = 'https://dipolack.github.io/WDD230/lessons/lesson9/data.json';
+const reqtURL = 'https://dipolack.github.io/wdd230/chamber/json/data.json';
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 const display = document.getElementById("cards");
