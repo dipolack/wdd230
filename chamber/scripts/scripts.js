c240404fc0b71c9c5fd0a9dc14c71dc9e24f1d2c
dipolack <pol22007@byui.edu>
2023-03-19 10:05:30 -0400

    finalizing

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -16,12 +16,19 @@ function toggleMenu() {
 
 const x = document.getElementById('hamburguerBtn');
 x.onclick = toggleMenu;
-let day = date.getDay();
 
-if (day == 1 || day == 2) {
+const day = new Date().getDay();
+const header = document.querySelector("header");
+
+const dayOfTheWeek = [
+    "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
+];
+
+if (dayOfTheWeek[day] == "Monday" || dayOfTheWeek [day]== "Tuesday") {
     let banner = document.querySelector('.banner');
     banner.style.display = 'block';
 } else {
     let banner = document.querySelector('.banner');
-    banner.style.display = '';
+    banner.style.display = 'none';
 }
+
